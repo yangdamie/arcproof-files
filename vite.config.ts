68bfd5da3442b8271867084a5a702f6069441cbb
YIXIN <136701058+yangdamie@users.noreply.github.com>
2026-07-04 15:27:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',          // ✅ 关键修改：从 '/' 改成 './'
   server: {
-    host: "127.0.0.1",
+    host: '::',
     port: 5173,
   },
-});
+})
